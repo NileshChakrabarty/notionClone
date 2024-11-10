7c586d652051c501b6d10ade1644e7412cc37d62
NileshChakrabarty <148804530+NileshChakrabarty@users.noreply.github.com>
2024-11-10 20:53:56 +0530

    Update SignupForm.js

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -20,7 +20,7 @@ const SignupForm = ({ handleSignup, closeModal, darkMode }) => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5001/api/signup', {
+      const response = await fetch('https://notion-clone-srever.vercel.app/api/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
